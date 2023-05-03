6afa0fbb5d6d0152569ccac26ae2a08ebb2c3e79
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2023-05-03 11:22:50 +0100

    Stripe checkout completed

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,25 @@
-import React, { Children } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Footer from './Footer'
+import { ProductsContext } from './ProductsContext'
 
 const Layout = ({children}) => {
+  const {setSelectedProducts} = useContext(ProductsContext);
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    if (window.location.href.includes('success')) {
+      setSelectedProducts([]);
+      setSuccess(true);
+    }
+  }, [])
   return (
     <>
       <div className='p-5'>
+        {success && (
+          <div className="mb-5 bg-green-400 text-white text-lg p-5 rounded-xl">
+            Thanks for your order!
+          </div>
+        )}
         {children}
       </div>
       <Footer/>
